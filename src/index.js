a6e9990e3238c347bf5380c323ba19ce03e54ab0
Izaquiel <zefcruzbs@hotmail.com>
2019-11-29 15:55:51 -0300

    Adicionado o Babel, video 6.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Webpack está funcionando!!!! :)')
+'use strict'
+
+import title from './app.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  React.createElement(title),
+  document.querySelector('[data-js="app"]')
+)
